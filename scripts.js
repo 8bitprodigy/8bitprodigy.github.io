17e94fc52f6af3861f5ba3d5f8ad277ec206f542
8bitprodigy <chrisxdeboy@gmail.com>
2024-08-09 03:55:18 -0700

    Removed unnecessary code.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-function registerVideoSelector(video_src, button, new_video) {
-    document.getElementById(button).addEventListener("click", function(event) {
-    document.getElementById(video_src).setAttribute('src', new_video);
-  }, false);
-}
-
 function changeVideoSrc(target_video, new_video){
   var video_element = document.getElementById(target_video)
   video_element.src = 'videos/' + new_video + '.mp4';

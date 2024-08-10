9e40965f7f5a62ee5fcb56eec7ee9a99a124b4bb
8bitprodigy <chrisxdeboy@gmail.com>
2024-08-09 17:05:12 -0700

    Added more images, as well as selectors to choose between them.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 function changeVideoSrc(target_video, new_video){
-  var video_element = document.getElementById(target_video)
+  var video_element = document.getElementById(target_video);
   video_element.src = 'videos/' + new_video + '.mp4';
   video_element.load();
+}
+
+function changeImgSrc(target_img, new_img){
+  var img_element = document.getElementById(target_img);
+  img_element.src = new_img;
+  img_element.load();
 }
